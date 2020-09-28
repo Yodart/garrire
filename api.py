3eff3176cb314a82b1fc4b704b14936b8b4bdf2c
Yodart <joaogabrielgcoutinho@gmail.com>
2020-09-28 01:04:03 -0300

    Pika Setup

--- api.py
+++ api.py
@@ -6,6 +6,7 @@ from flask_socketio import SocketIO, join_room
 from db import db_connect
 import os
 import sys
+import pika
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
@@ -20,17 +21,23 @@ def join(data):
     join_room(data['room'])
     socketio.emit("join", data)
 
-
 @socketio.on('message')
 @db_connect
 def message(db_cursor, db_connection, data):
-    print(data['room'])
     db_cursor.execute(
         "INSERT INTO messages (username,content,room) values(%s,%s,%s)", (data['username'], data['message'], data['room']))
     db_connection.commit()
     socketio.emit("message", data, room=data['room'])
     db_cursor.close()
     db_connection.close()
+    message = data['message']
+    if message.startswith('/stock=') and len(message)>7:
+        stock = message.replace('/stock=','')
+        connection = pika.BlockingConnection(pika.ConnectionParameters('http://localhost:3000/'))
+        channel = connection.channel()
+        channel.queue_declare(queue='stock')
+        channel.basic_publish(exchange='',routing_key='stock-key',body=stock)
+        connection.close()
 
 
 if __name__ == '__main__':
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/b/Desktop/jobsity-chat/venv"
+set -gx VIRTUAL_ENV "/Users/b/Documents/GitHub/jobsity-chat/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
