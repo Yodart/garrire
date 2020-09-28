7483a572e7dceb281c6b4643f753cff0262bee71
Yodart <joaogabrielgcoutinho@gmail.com>
2020-09-28 07:40:44 -0300

    Removed Bot Message Db Storing

--- api.py
+++ api.py
@@ -17,9 +17,8 @@ app.register_blueprint(rooms)
 
 
 # Endpoint to be consumed by the StockBot to publish messages to a direct room
-# it uses a secretkey to ensure security and after checking for that it adds
-# the message to the database and the emmits the socket io message to be render
-# on the client side.
+# it uses a secretkey to ensure security and after checking for that it emmits 
+# the socket io message to be rendered on the client side.
 
 @app.route('/message', methods=['POST'])
 @db_connect
@@ -27,12 +26,7 @@ def message(db_cursor, db_connection):
     try:
         data = request.json
         if data['secret']=='jobsitystockbotkey':
-            db_cursor.execute(
-                "INSERT INTO messages (username,content,room) values(%s,%s,%s)", (data['username'], data['message'], data['room']))
-            db_connection.commit()
             socketio.emit("message", data, room=data['room'])
-            db_cursor.close()
-            db_connection.close()
             return {'response': 'Message sent'},200
         return{'response': 'Insufficient permissions'}, 401
     except:
@@ -48,15 +42,21 @@ def join(data):
     socketio.emit("join", data)
 
 
-# SocketIO callback for when a the client sends a message in a chat room. It emits back a 
-# message to the client that will there be consumed and rendered. Finally it checks if the
-# message has the botmessage format and if so adds that job onto the bot RabbitMQ queu that
-# will be consumed and ultimately processed by the bot to post a message on the channel.
+# SocketIO callback for when a the client sends a message in a chat room. It stores
+# that message record on the database and then emits back a message to the client that
+# will there be consumed and rendered. Finally it checks if the message has the bot
+# message format and if so adds that job onto the bot RabbitMQ queu that will be consumed
+# and ultimately processed by the bot to post a message on the channel.
 
 @socketio.on('message')
 @db_connect
 def message(db_cursor, db_connection, data):
+    db_cursor.execute(
+        "INSERT INTO messages (username,content,room) values(%s,%s,%s)", (data['username'], data['message'], data['room']))
+    db_connection.commit()
     socketio.emit("message", data, room=data['room'])
+    db_cursor.close()
+    db_connection.close()
     message = data['message']
     if message.startswith('/stock=') and len(message)>7:
         try:
