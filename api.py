8883cbfb7ee9e1bdca62dfce6f268771058d891e
Yodart <joaogabrielgcoutinho@gmail.com>
2020-09-28 07:37:03 -0300

    Remove Message Storing for the Bot Messages

--- api.py
+++ api.py
@@ -48,21 +48,15 @@ def join(data):
     socketio.emit("join", data)
 
 
-# SocketIO callback for when a the client sends a message in a chat room. It stores
-# that message record on the database and then emits back a message to the client that
-# will there be consumed and rendered. Finally it checks if the message has the bot
-# message format and if so adds that job onto the bot RabbitMQ queu that will be consumed
-# and ultimately processed by the bot to post a message on the channel.
+# SocketIO callback for when a the client sends a message in a chat room. It emits back a 
+# message to the client that will there be consumed and rendered. Finally it checks if the
+# message has the botmessage format and if so adds that job onto the bot RabbitMQ queu that
+# will be consumed and ultimately processed by the bot to post a message on the channel.
 
 @socketio.on('message')
 @db_connect
 def message(db_cursor, db_connection, data):
-    db_cursor.execute(
-        "INSERT INTO messages (username,content,room) values(%s,%s,%s)", (data['username'], data['message'], data['room']))
-    db_connection.commit()
     socketio.emit("message", data, room=data['room'])
-    db_cursor.close()
-    db_connection.close()
     message = data['message']
     if message.startswith('/stock=') and len(message)>7:
         try:
