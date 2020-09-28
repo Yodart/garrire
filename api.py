5e44ee1562968b1709a2dc81a59e01d16f6db4f2
Yodart <joaogabrielgcoutinho@gmail.com>
2020-09-28 04:11:46 -0300

    Stock Bot

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify,request
 from users import users
 from auth import auth
 from rooms import rooms
@@ -15,6 +15,22 @@ app.register_blueprint(auth)
 app.register_blueprint(users)
 app.register_blueprint(rooms)
 
+@app.route('/message', methods=['POST'])
+@db_connect
+def message(db_cursor, db_connection):
+    try:
+        data = request.json
+        if data['secret']=='jobsitystockbotkey':
+            db_cursor.execute(
+                "INSERT INTO messages (username,content,room) values(%s,%s,%s)", (data['username'], data['message'], data['room']))
+            db_connection.commit()
+            socketio.emit("message", data, room=data['room'])
+            db_cursor.close()
+            db_connection.close()
+            return {'response': 'Message sent'},200
+        return{'response': 'Insufficient permissions'}, 401
+    except:
+        return {'response': 'Unable to send message. Insufficient or wrong data provided'},401
 
 @socketio.on('join')
 def join(data):
@@ -32,12 +48,15 @@ def message(db_cursor, db_connection, data):
     db_connection.close()
     message = data['message']
     if message.startswith('/stock=') and len(message)>7:
-        stock = message.replace('/stock=','')
-        connection = pika.BlockingConnection(pika.ConnectionParameters('http://localhost:3000/'))
-        channel = connection.channel()
-        channel.queue_declare(queue='stock')
-        channel.basic_publish(exchange='',routing_key='stock-key',body=stock)
-        connection.close()
+        try:
+            stock = message.replace('/stock=','')
+            connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+            channel = connection.channel()
+            channel.queue_declare(queue='stock-query')
+            channel.basic_publish(exchange='',routing_key='stock-query',body="{}|{}".format(stock,data['room']))
+            connection.close()
+        except:
+            print(str(sys.exc_info()))
 
 
 if __name__ == '__main__':
